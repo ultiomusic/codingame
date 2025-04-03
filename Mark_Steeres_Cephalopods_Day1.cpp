d32ad8282978cd94b4849e61c9edcae2618ea656
Ultio. <94832543+ultiomusic@users.noreply.github.com>
2025-04-03 23:02:53 +0000

    19142ms

--- Mark_Steeres_Cephalopods_Day1.cpp
+++ Mark_Steeres_Cephalopods_Day1.cpp
@@ -8,47 +8,35 @@ using namespace std;
 
 const int MOD = 1 << 30;
 const int GRID_SIZE = 3;
-
-vector<vector<int>> hash_to_grid(long long hash_value) {
-    vector<vector<int>> grid(GRID_SIZE, vector<int>(GRID_SIZE));
-    for (int i = GRID_SIZE - 1; i >= 0; --i) {
-        for (int j = GRID_SIZE - 1; j >= 0; --j) {
-            grid[i][j] = hash_value % 10;
-            hash_value /= 10;
-        }
-    }
-    return grid;
-}
+const long long pow10[3][3] = {
+    {100000000LL, 10000000LL, 1000000LL},
+    {100000LL, 10000LL, 1000LL},
+    {100LL, 10LL, 1LL}
+};
 
 long long grid_to_hash(const vector<vector<int>>& grid) {
-    long long hash_value = 0;
+    long long hash = 0;
     for (int i = 0; i < GRID_SIZE; ++i) {
         for (int j = 0; j < GRID_SIZE; ++j) {
-            hash_value = hash_value * 10 + grid[i][j];
+            hash += grid[i][j] * pow10[i][j];
         }
     }
-    return hash_value;
+    return hash;
 }
 
-bool is_full(const vector<vector<int>>& grid) {
-    for (const auto& row : grid) {
-        for (int cell : row) {
-            if (cell == 0) {
-                return false;
-            }
-        }
+bool is_full(long long hash) {
+    for (int i = 0; i < 9; ++i) {
+        if (hash % 10 == 0) return false;
+        hash /= 10;
     }
     return true;
 }
 
-vector<pair<int, int>> get_adjacent_cells(int i, int j) {
-    vector<pair<int, int>> adjacent;
-    if (i > 0) adjacent.emplace_back(i - 1, j);
-    if (i < GRID_SIZE - 1) adjacent.emplace_back(i + 1, j);
-    if (j > 0) adjacent.emplace_back(i, j - 1);
-    if (j < GRID_SIZE - 1) adjacent.emplace_back(i, j + 1);
-    return adjacent;
-}
+const vector<pair<int, int>> adjacency[3][3] = {
+    {{{0,1}, {1,0}}, {{0,0}, {0,2}, {1,1}}, {{0,1}, {1,2}}},
+    {{{1,1}, {0,0}, {2,0}}, {{1,0}, {1,2}, {0,1}, {2,1}}, {{1,1}, {0,2}, {2,2}}},
+    {{{2,1}, {1,0}}, {{2,0}, {2,2}, {1,1}}, {{2,1}, {1,2}}}
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -72,80 +60,70 @@ int main() {
 
     for (int move = 0; move <= depth; ++move) {
         for (const auto& [board_hash, count] : dp_current) {
-            vector<vector<int>> current_grid = hash_to_grid(board_hash);
-
-            if (is_full(current_grid)) {
-                final_sum = (final_sum + board_hash * count) % MOD;
-                continue;
-            }
-
-            if (move == depth) {
+            if (is_full(board_hash) || move == depth) {
                 final_sum = (final_sum + board_hash * count) % MOD;
                 continue;
             }
 
             vector<pair<int, int>> empty_positions;
-            for (int i = 0; i < GRID_SIZE; ++i) {
-                for (int j = 0; j < GRID_SIZE; ++j) {
-                    if (current_grid[i][j] == 0) {
-                        empty_positions.emplace_back(i, j);
-                    }
+            long long tmp_hash = board_hash;
+            for (int k = 0; k < 9; ++k) {
+                int digit = tmp_hash % 10;
+                if (digit == 0) {
+                    int i = 2 - (k / 3);
+                    int j = 2 - (k % 3);
+                    empty_positions.emplace_back(i, j);
                 }
+                tmp_hash /= 10;
             }
 
             for (const auto& pos : empty_positions) {
                 int i = pos.first;
                 int j = pos.second;
-                vector<pair<int, int>> adjacent_cells = get_adjacent_cells(i, j);
+                const auto& adjacent_cells = adjacency[i][j];
                 vector<pair<int, int>> adj_non_zero;
                 for (const auto& cell : adjacent_cells) {
                     int x = cell.first;
                     int y = cell.second;
-                    if (current_grid[x][y] != 0) {
+                    long long cell_value = (board_hash / pow10[x][y]) % 10;
+                    if (cell_value != 0) {
                         adj_non_zero.emplace_back(x, y);
                     }
                 }
 
                 if (adj_non_zero.size() < 2) {
-                    vector<vector<int>> new_grid = current_grid;
-                    new_grid[i][j] = 1;
-                    long long new_hash = grid_to_hash(new_grid);
+                    long long new_hash = board_hash + pow10[i][j];
                     dp_next[new_hash] += count;
                 } else {
                     int n = adj_non_zero.size();
                     bool has_valid_subset = false;
-                    for (int mask = 1; mask < (1 << n); ++mask) {
-                        int sum_subset = 0;
-                        int bits = __builtin_popcount(mask);
-                        if (bits < 2) continue;
+                    for (int mask = 3; mask < (1 << n); ++mask) { // Start from 3 (binary 11) to ensure at least 2 bits set
+                        if (__builtin_popcount(mask) < 2) continue;
+                        long long subset_sum = 0;
+                        long long new_hash = board_hash;
+                        bool valid = true;
                         for (int k = 0; k < n; ++k) {
                             if (mask & (1 << k)) {
-                                sum_subset += current_grid[adj_non_zero[k].first][adj_non_zero[k].second];
+                                int x = adj_non_zero[k].first;
+                                int y = adj_non_zero[k].second;
+                                long long digit = (board_hash / pow10[x][y]) % 10;
+                                subset_sum += digit;
+                                new_hash -= digit * pow10[x][y];
                             }
                         }
-                        if (sum_subset <= 6) {
-                            has_valid_subset = true;
-                            vector<vector<int>> new_grid = current_grid;
-                            for (int k = 0; k < n; ++k) {
-                                if (mask & (1 << k)) {
-                                    new_grid[adj_non_zero[k].first][adj_non_zero[k].second] = 0;
-                                }
-                            }
-                            new_grid[i][j] = sum_subset;
-                            long long new_hash = grid_to_hash(new_grid);
-                            dp_next[new_hash] += count;
-                        }
+                        if (subset_sum > 6) continue;
+                        has_valid_subset = true;
+                        new_hash += subset_sum * pow10[i][j];
+                        dp_next[new_hash] += count;
                     }
                     if (!has_valid_subset) {
-                        vector<vector<int>> new_grid = current_grid;
-                        new_grid[i][j] = 1;
-                        long long new_hash = grid_to_hash(new_grid);
+                        long long new_hash = board_hash + pow10[i][j];
                         dp_next[new_hash] += count;
                     }
                 }
             }
         }
-        dp_current = dp_next;
+        dp_current.swap(dp_next);
         dp_next.clear();
     }
 
